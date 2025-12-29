8e98f0653476216bf91deaf9c69d946194c71576
rockybanna <ajayrajpoot420@gmail.com>
2025-12-30 00:51:45 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,47 +1,8 @@
-const CACHE_NAME = "foodxpress-v3";
-
-/* Cache ONLY static files */
-const STATIC_FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./company-logo.png",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
+// Service Worker temporarily disabled for stability
 self.addEventListener("install", event => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(STATIC_FILES))
-  );
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(k => {
-          if (k !== CACHE_NAME) return caches.delete(k);
-        })
-      )
-    )
-  );
   self.clients.claim();
-});
-
-/* VERY IMPORTANT:
-   Do NOT intercept Google Apps Script API calls */
-self.addEventListener("fetch", event => {
-  const url = event.request.url;
-
-  /* Always go to network for Apps Script */
-  if (url.includes("script.google.com")) {
-    return;
-  }
-
-  /* Cache-first for static files only */
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
 });
